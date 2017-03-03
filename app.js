0e39040a43bf5390eb8f0f85999013e267718ea1
iastanley <iasvks.craigslist@gmail.com>
2017-03-02 17:08:34 -0800

    added event handling to next and prev buttons - still not working

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 $(document).ready(function(){
   var YOUTUBE_SEARCH_LIST_ENDPOINT = 'https://www.googleapis.com/youtube/v3/search';
+  var state = {
+    prevToken: '',
+    nextToken: 0
+  };
 
   //Make ajax request on form submit
   formHandler();
@@ -7,7 +11,7 @@ $(document).ready(function(){
   /********FUNCTIONS********/
 
   //Making AJAX request
-  function getDataFromApi(searchTerm, callback) {
+  function getDataFromSearch(searchTerm, callback) {
     var settings = {
       url: YOUTUBE_SEARCH_LIST_ENDPOINT,
       data: {
@@ -23,9 +27,34 @@ $(document).ready(function(){
     $.ajax(settings);
   }
 
+  function getDataFromPage(token, callback) {
+    var settings = {
+      url: YOUTUBE_SEARCH_LIST_ENDPOINT,
+      data: {
+        part: 'snippet',
+        key: 'AIzaSyCbOY5vCwchBe7kOR4tQ0APZfVuSpLdEHE',
+        maxResults: 6,
+        pageToken: token
+      },
+      datatype: 'json',
+      type: 'GET',
+      success: callback
+    }
+    $.ajax(settings);
+  }
+
+  //ask question about this
+  function storePageData(data) {
+    var prev = data.prevPageToken;
+    var next = data.nextPageToken;
+    state.prevToken = prev;
+    state.nextToken = next;
+    console.log(state.nextToken);
+  }
+
   //Render return data object to html
   function showApiData(data) {
-    console.log(data);
+    showNavButtons(data);
     var resultsHTML = '';
     var videoURL = '';
     var videoId = '';
@@ -36,14 +65,54 @@ $(document).ready(function(){
       resultsHTML += '<img src="' + videoURL + '"></a>';
     }
     $('#js-search-results').html(resultsHTML);
+    navButtonHandler(data);
+    console.log(data);
+  }
+
+  function showNavButtons(data){
+    //show navigation buttons
+    $('.prev-next-btns').show();
+    //disable previous button if there are no previous
+    if (!data.prevPageToken) {
+      $('#prev-btn').addClass('disabled');
+    } else {
+      $('#prev-btn').removeClass('disabled');
+    }
+    //disable next button if there are no next
+    if (!data.nextPageToken) {
+      $('#next-btn').addClass('disabled');
+    } else {
+      $('#next-btn').removeClass('disabled');
+    }
+  }
+
+  //navigation button handling
+  function navButtonHandler(data){
+    //next button
+    if (data.nextPageToken) {
+      $('#js-search-form').on('click', '#next-btn', function(){
+        getDataFromPage(data.nextPageToken, showApiData);
+      });
+    }
+    //previous button
+    if (data.prevPageToken) {
+      $('#js-search-form').on('click', '#prev-btn', function(){
+        getDataFromPage(data.prevPageToken, showApiData);
+      });
+    }
   }
 
   //event listener for form includes making ajax request
   function formHandler() {
     $('#js-search-form').submit(function(event){
       event.preventDefault();
+
+      //get input from user
       var query = $(this).find('.js-query').val();
-      getDataFromApi(query, showApiData);
+      getDataFromSearch(query, showApiData);
+      navButtonHandler();
     });
   }
+
+
 });
